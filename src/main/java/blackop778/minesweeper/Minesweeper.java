7532ad324338ecee7c3b0b5de369499c9dec6206
Nathan <NSFaltermeier@bluevalleyk12.net>
2016-05-17 13:34:23 -0500

    Swing is confusing
    Also added Gradle

---
+++ src/main/java/blackop778/minesweeper/Minesweeper.java
@@ -0,0 +1,9 @@
+package blackop778.minesweeper;
+
+public class Minesweeper
+{
+	public static void main(String[] args)
+	{
+
+	}
+}
---
+++ src/main/java/javax/swing/MinesweeperPanel.java
@@ -0,0 +1,31 @@
+package javax.swing;
+
+import java.awt.LayoutManager;
+
+public class MinesweeperPanel extends JPanel
+{
+
+	public MinesweeperPanel()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public MinesweeperPanel(LayoutManager arg0)
+	{
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MinesweeperPanel(boolean arg0)
+	{
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MinesweeperPanel(LayoutManager arg0, boolean arg1)
+	{
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+}
